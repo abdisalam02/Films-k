dfe03e02834264e9eb1f58bc0b91fe93fd77de70
abdisalam02 <niwache12@gmail.com>
2023-03-30 02:29:48 +0200

    final final

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import React, { useState, useEffect} from 'react';
 import MovieList from './components/MovieCard';
 import SearchBar from './components/SearchResults';
 
-// const API_URL = 'http://www.omdbapi.com?apikey=dba6a593';
+
 const KEY = 'dba6a593';
 const URL = 'https://www.omdbapi.com/';
 
@@ -16,18 +16,24 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('');
   // const[info, setInfo] = useState([]);
 
+  // her så bruker jeg useeffect til å hente en liste med 10 james bond filmer 
   useEffect(() => {
     async function fetchMovies() {
       const url = `https://www.omdbapi.com/?s=james+bond&apikey=dba6a593&type=movie`;
       const response = await fetch(url);
       const data = await response.json();
       // const movieResults = data.Search.filter((result) => result.Type === "movie");
+    // henter extra informasjon som actors, genre og directors
       if (data.Search) {
-        const movies = await Promise.all(data.Search.slice(0, 10).map(async (movie) => {
-          const url = `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${KEY}`;
+        const movies = await Promise.all
+        (data.Search.slice(0, 10).map(async (movie) => {
+
+          const url = `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=dba6a593`;
           const response = await fetch(url);
           const data = await response.json();
+          
           return data;
+
         }));
         setMovies(movies);
       }
@@ -42,7 +48,9 @@ function App() {
 
   const SearchSubmit = async (event) => {
     event.preventDefault();
+    //bruker en if test som returnerer resultater hvis brukeren har skrevet minimum 3 tegn 
     if (searchTerm.length >= 2) {
+
       const url = `${URL}?s=${searchTerm}&apikey=${KEY}`;
       const response = await fetch(url);
       const data = await response.json();
@@ -53,6 +61,7 @@ function App() {
         const movieUrl = `${URL}?i=${movie.imdbID}&apikey=${KEY}`;
         const movieResponse = await fetch(movieUrl);
         const movieData = await movieResponse.json();
+
         return movieData;
 
       }));
@@ -60,7 +69,7 @@ function App() {
       setMovies(movies);
 
       } else {
-        
+      
         setMovies([]);
       }
     }
